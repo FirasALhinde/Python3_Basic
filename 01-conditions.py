38588156b2e1ab67a4d4359adeffd426b19676fd
unknown <you@example.com>
2022-12-20 01:24:23 +0200

    add python conditions

---
+++ 01-conditions.py
@@ -0,0 +1,13 @@
+#simple condtion
+x = 3
+if x>3:
+    print('x>3')
+elif x == 3:
+    print('x==3')
+else:
+   print(x)
+
+name = 'firas'
+age = 24
+if name =='firas' and age ==24:
+    print(f'Hello {name}')
