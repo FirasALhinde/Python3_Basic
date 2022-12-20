0add99858f21b69823a9671cab3f58c2976b4648
unknown <you@example.com>
2022-12-20 23:51:06 +0200

    add text loops.py

---
+++ 02-loops.py
@@ -0,0 +1,69 @@
+'''
+x = 0
+while x < 10: print(x) ;  x+=1
+'''
+'''
+x = 0
+while x < 10:
+    print(x)
+    y=0
+    while y<4:
+        print(y)
+        y+=1
+    x+=1
+'''
+'''
+x=1
+while x <= 10:
+    y=1
+    while y<=10:
+        print(f'{x} * {y} = {x*y}')
+        y+=1
+    x+=1
+'''
+
+'''
+x = 1
+while x<=10:
+    print(f'1 * {x} = {x*1}')
+    x+=1
+'''
+'''
+name = 'firas'
+for lat in name:
+    print(lat)
+'''
+'''
+for num in range(1,11):
+    print(num,end=' ')
+'''
+'''
+for num in range(1,11):
+    for n in range(1,11):
+        print(f'{num} * {n} = {num*n}')
+'''
+'''
+x = int(input('enter the numbers\n'))
+
+for n in range(1,x+1):
+    print(n)
+'''
+'''
+for n in range(10):
+    if n == 6:
+        break
+    print(n)
+
+else:
+    print('welcome')
+'''
+
+'''
+for n in range(10):
+    if n == 6:
+        continue
+    print(n)
+
+else:
+'''
+
