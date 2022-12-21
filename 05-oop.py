988fa4772ef981c6a6d2876c8279e16ca5c6cb53
unknown <you@example.com>
2022-12-22 00:47:58 +0200

    add file oop1

--- 05-oop.py
+++ 05-oop.py
@@ -1,3 +1,15 @@
-x = [1,2,3,4,5]
-a,*b=x
-print(b)
+class Calculator:
+    def __init__(self,x,y,name):
+        self.x = x
+        self.y = y
+        print(f'Hello {name}')
+        print('in __init__ :: ',self.sum())
+    def sum(self):
+        return self.x+self.y
+    def multi(self):
+        return self.x*self.y
+
+s = Calculator(12,12,'firas')
+print(s.sum())
+print(s.multi())
+    
