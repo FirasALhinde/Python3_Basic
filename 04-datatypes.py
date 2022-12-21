b22ee5f0b5282686e42f1244384fd7a4f5396f4a
unknown <you@example.com>
2022-12-21 18:37:08 +0200

    edit file datatypes

---
+++ 04-datatypes.py
@@ -0,0 +1,20 @@
+'''
+s = {'name':'firas','age':24}
+
+for k  in s:
+    print(f'{k} -> {s[k]}')
+'''
+'''
+def sum():
+xx*2
+xreturn 12+12
+
+print(sum())
+help(sum)
+'''
+name = "firas\
+alhinde\
+abo\
+khaldoun"
+
+print(name)
